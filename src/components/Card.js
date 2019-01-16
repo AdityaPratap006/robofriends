ce63d37679861aae77afce53e97f00e505721566
AdityaPratap006 <40934015+AdityaPratap006@users.noreply.github.com>
2019-01-16 21:18:57 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = ({id , name, email}) => {
 	return (
 
 		<div className='bg-light-green dib br3 pa3  ma2 grow bw2 shadow-5 '>
-			<img className='car' alt='robots' src={`https://images.caricos.com/r/rolls-royce/2015_rolls-royce_ghost_series_ii/images/1600x1200/2015_rolls-royce_ghost_series_ii_${id}_1600x1200.jpg`}/>
+			<img className='car' alt='robots' src={`https://robohash.org/${id}?size=300x400`}/>
 			<div className='tc '>
 				<h3>{name}</h3>
 				<p >{email}</p>
