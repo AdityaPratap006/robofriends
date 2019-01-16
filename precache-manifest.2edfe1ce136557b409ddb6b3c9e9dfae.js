38fbec64f3ed714c0fca4f722e2629109eda1c52
Aditya Pratap <adityapratap7344@gmail.com>
2019-01-16 11:39:51 +0530

    Updates

---
+++ precache-manifest.2edfe1ce136557b409ddb6b3c9e9dfae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde070e9efb3f6600f7482b9b3234e85",
+    "url": "/AdityaPratap006/robofriends/static/media/logo.dde070e9.png"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/AdityaPratap006/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ca20a9f3a06d5146ac55",
+    "url": "/AdityaPratap006/robofriends/static/js/runtime~main.ca20a9f3.js"
+  },
+  {
+    "revision": "f98a358ac823222ce192",
+    "url": "/AdityaPratap006/robofriends/static/js/main.f98a358a.chunk.js"
+  },
+  {
+    "revision": "42c2d917a2b28d7c9ff7",
+    "url": "/AdityaPratap006/robofriends/static/js/1.42c2d917.chunk.js"
+  },
+  {
+    "revision": "f98a358ac823222ce192",
+    "url": "/AdityaPratap006/robofriends/static/css/main.7f5bbef6.chunk.css"
+  },
+  {
+    "revision": "42c2d917a2b28d7c9ff7",
+    "url": "/AdityaPratap006/robofriends/static/css/1.c4c03c36.chunk.css"
+  },
+  {
+    "revision": "78c006a17606cc6cd21868106c71b6ed",
+    "url": "/AdityaPratap006/robofriends/index.html"
+  }
+];
